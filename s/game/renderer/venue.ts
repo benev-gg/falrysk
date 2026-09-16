be7217a41ea775d7e75a56902c25f2d9f6f02ef2
Chase Moskal <chasemoskal@gmail.com>
2026-09-15 22:02:21 -0700

    sketch: director2

--- s/game/renderer/venue.ts
+++ s/game/renderer/venue.ts
@@ -2,14 +2,16 @@
 import {EntitiesReadonly} from "@benev/archimedes"
 import {createEngine, createSceneContext, disposeEngine, disposeScene} from "@babylonjs/lite"
 
+import {Catalog} from "./catalog.js"
 import {PlayerId} from "../simulation/types.js"
 import {GameComponents} from "../simulation/parts/components.js"
 
 export type Venue = Awaited<ReturnType<typeof setupVenue>>
 
-export async function setupVenue({playerId, entities}: {
+export async function setupVenue({playerId, entities, catalog}: {
 		playerId: PlayerId,
 		entities: EntitiesReadonly<GameComponents>,
+		catalog: Catalog
 	}) {
 
 	const canvas = document.createElement("canvas")
@@ -24,6 +26,6 @@ export async function setupVenue({playerId, entities}: {
 		disposeEngine(engine)
 	}
 
-	return {playerId, entities, canvas, engine, scene, dispose}
+	return {playerId, entities, canvas, engine, scene, catalog, dispose}
 }
 
