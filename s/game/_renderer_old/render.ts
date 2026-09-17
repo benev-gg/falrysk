a84a3cfb776fc9effa4d1098f96100de56e42a09
Chase Moskal <chasemoskal@gmail.com>
2026-09-17 00:28:27 -0700

    delete: obsolete code

--- s/game/_renderer_old/render.ts
+++
@@ -1,9 +0,0 @@
-
-import {consolidate} from "@benev/archimedes"
-import {Realm} from "./realm.js"
-import {timing_update} from "./systems/timing_update.js"
-
-export const setupRender = (realm: Realm) => consolidate(realm, {
-	timing_update,
-})
-
